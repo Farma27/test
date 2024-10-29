0bbc5ff158c434da1c21380f35b61a098302bd85
Farma27 <farmafnr12@gmail.com>
2024-10-29 22:08:47 +0700

    update

--- Belajar Membuat Aplikasi Back-End untuk Pemula dengan Google Cloud/nodejs-web-server/server.js
+++ Belajar Membuat Aplikasi Back-End untuk Pemula dengan Google Cloud/nodejs-web-server/server.js
@@ -10,15 +10,58 @@ const host = 'localhost'
  * @param response: objek yang digunakan untuk menanggapi permintaan
  */
 const requestListener = (request, response) => {
-    const { method } = request
+    const { method, url } = request
 
     response.setHeader('Content-Type', 'text/html')
-    response.statusCode = 200
+    response.setHeader('X-Powered-By', 'NodeJS')
 
-    if (method === 'GET') {
-        response.end('<h1>Hello!</h1>')
+    if (url === '/') {
+        if (method === 'GET') {
+            response.statusCode = 200
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }))
+        } else {
+            response.statusCode = 400
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }))
+        }
+    } else if (url === '/about') {
+        if (method === 'GET') {
+            response.statusCode = 200
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }))
+        } else if (method === 'POST') {
+            let body = []
+
+            request.on('data', (chunk) => {
+                body.push(chunk)
+            })
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString()
+                const { name } = JSON.parse(body)
+                response.statusCode = 200
+                response.end(JSON.stringify({
+                    message: `Halo, ${name}! Ini adalah halaman about`,
+                }))
+            })
+        } else {
+            response.statusCode = 400
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses menggunakan ${method}, request`
+            }))
+        }
+    } else {
+        response.statusCode = 404
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }))
     }
 
+    /* 
     if (method === 'POST') {
         let body = []
 
@@ -33,7 +76,6 @@ const requestListener = (request, response) => {
         })
     }
 
-    /* 
     if (method === 'PUT') {
         response.end('<h1>Bonjour!</h1>')
     }

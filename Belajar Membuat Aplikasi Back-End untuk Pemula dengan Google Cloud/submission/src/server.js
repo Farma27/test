ace48d502e9b394aacb59830256fd28b4bbff382
Farma27 <farmafnr12@gmail.com>
2024-11-10 23:53:43 +0700

    fix

--- Belajar Membuat Aplikasi Back-End untuk Pemula dengan Google Cloud/submission/src/server.js
+++ Belajar Membuat Aplikasi Back-End untuk Pemula dengan Google Cloud/submission/src/server.js
@@ -4,11 +4,10 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 9000,
-    host:
-      process.env.NODE_ENV === 'production' ? 'localhost' : '0.0.0.0',
-    route: {
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
       cors: {
-        origin: ['*']
+        origin: ['*'],
       },
     },
   });
